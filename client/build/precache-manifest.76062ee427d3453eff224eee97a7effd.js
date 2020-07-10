b3f7d9a4f306c51277c484be23ae96e55d64ba66
JoyJooy <imli.mengyuan@gmail.com>
2020-07-10 13:09:53 -0400

    REMOVE BUILD FROM GIT IGNORE

---
+++ client/build/precache-manifest.76062ee427d3453eff224eee97a7effd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4de56b1e794b54a377f5bfa3d6e85a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e039eb052847e5fc63f7",
+    "url": "/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "e039eb052847e5fc63f7",
+    "url": "/static/js/2.9f6f4bbe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9f6f4bbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dd1be7cefadb533f159",
+    "url": "/static/js/main.1dd8dbf3.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
